f362009a3f96626d286267f5c070daa3429b6ace
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-21 20:20:28 -0300

    feat: enhance login screen with input fields and improved layout

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,11 +1,31 @@
 import { router } from 'expo-router'
-import { Text, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
+import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import Logo from '@/assets/logo.svg'
+import { Button, ButtonIcon, ButtonText } from '@/components/ui/button'
+import {
+  AccessIcon,
+  ArrowRight02Icon,
+  Mail02Icon,
+  ViewIcon,
+  ViewOffIcon,
+} from '@/components/ui/icon'
+import {
+  Input,
+  InputField,
+  InputIcon,
+  InputRightIcon,
+  InputSlot,
+} from '@/components/ui/input'
+import { VStack } from '@/components/ui/vstack'
 import { useSession } from '@/contexts/auth-context'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
   const { signIn } = useSession()
 
   function login() {
@@ -19,13 +39,60 @@ export default function Login() {
 
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Logo width={120} height={40} />
-      <TouchableOpacity onPress={login}>
-        <Text className="font-title-lg">Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={register}>
-        <Text className="font-title-md">Register</Text>
-      </TouchableOpacity>
+      <VStack className="w-full flex-1 items-center justify-between p-[40px]">
+        <VStack className="w-full items-center gap-[61px]">
+          <VStack className="w-full items-center gap-[32px]">
+            <Logo width={64} height={48} />
+            <VStack className="w-full items-center gap-[8px]">
+              <Text className="font-title-lg text-gray-500">
+                Acesse sua conta
+              </Text>
+              <Text className="font-body-sm text-gray-300">
+                Informe seu e-mail e senha para entrar
+              </Text>
+            </VStack>
+          </VStack>
+          <VStack className="w-full gap-[40px]">
+            <VStack className="w-full gap-[20px]">
+              <Input isFilled={email.length > 0} label="E-mail">
+                <InputIcon as={Mail02Icon} />
+                <InputField
+                  placeholder="mail@exemplo.br"
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+              </Input>
+              <Input isFilled={password.length > 0} label="Senha">
+                <InputIcon as={AccessIcon} />
+                <InputField
+                  placeholder="Sua senha"
+                  value={password}
+                  onChangeText={setPassword}
+                  type={showPassword ? 'text' : 'password'}
+                />
+                <InputSlot onPress={() => setShowPassword(!showPassword)}>
+                  <InputRightIcon as={showPassword ? ViewOffIcon : ViewIcon} />
+                </InputSlot>
+              </Input>
+            </VStack>
+            <Button variant="solid" onPress={login}>
+              <ButtonText>Acessar</ButtonText>
+              <ButtonIcon as={ArrowRight02Icon} />
+            </Button>
+          </VStack>
+        </VStack>
+        <VStack className="w-full gap-[20px]">
+          <Text className="font-body-md text-gray-300">
+            Ainda não tem uma conta?
+          </Text>
+          <Button variant="outline" onPress={register}>
+            <ButtonText>Cadastrar</ButtonText>
+            <ButtonIcon as={ArrowRight02Icon} />
+          </Button>
+        </VStack>
+      </VStack>
     </SafeAreaView>
   )
 }
