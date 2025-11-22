13c5476c9bbe66e0b95c1d79621ca2cb0135c894
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-22 00:15:41 -0300

    feat: simplify HomeLayout by removing unnecessary Stack.Screen components

--- src/app/(app)/home/_layout.tsx
+++ src/app/(app)/home/_layout.tsx
@@ -1,10 +1,5 @@
 import { Stack } from 'expo-router'
 
 export default function HomeLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="product" />
-    </Stack>
-  )
+  return <Stack screenOptions={{ headerShown: false }} />
 }
