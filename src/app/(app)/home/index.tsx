097e4c98f69f1da0c51c48da0bf5a616fc88a908
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-22 01:28:27 -0300

    fix: update product data structure to use ProductCardProps and adjust keyExtractor

--- src/app/(app)/home/index.tsx
+++ src/app/(app)/home/index.tsx
@@ -1,72 +1,65 @@
 import { FlatList, ListRenderItem } from 'react-native'
 
-import { ProductCard } from '@/components/product-card'
-
-type Product = {
-  id: string
-  productPriceInCents: string
-  productTitle: string
-  productImageUri: string
-}
+import { ProductCard, ProductCardProps } from '@/components/product-card'
 
 // Mock data - replace with API call
-const PRODUCTS: Product[] = [
+const PRODUCTS: ProductCardProps[] = [
   {
-    id: '1',
+    productId: '1',
     productPriceInCents: '120090',
     productTitle: 'Sofá',
     productImageUri: 'product-1',
   },
   {
-    id: '2',
+    productId: '2',
     productPriceInCents: '3589',
     productTitle: 'Camiseta Masculina',
     productImageUri: 'product-2',
   },
   {
-    id: '3',
+    productId: '3',
     productPriceInCents: '8679',
     productTitle: 'Kit utensílios',
     productImageUri: 'product-3',
   },
   {
-    id: '4',
+    productId: '4',
     productPriceInCents: '15990',
     productTitle: 'Kit de cremes',
     productImageUri: 'product-4',
   },
   {
-    id: '5',
+    productId: '5',
     productPriceInCents: '5600',
     productTitle: 'Caderno de desenho',
     productImageUri: 'product-5',
   },
   {
-    id: '6',
+    productId: '6',
     productPriceInCents: '2460',
     productTitle: 'Carro de brinquedo',
     productImageUri: 'product-6',
   },
   {
-    id: '7',
+    productId: '7',
     productPriceInCents: '120090',
     productTitle: 'Sofá',
     productImageUri: 'product-1',
   },
   {
-    id: '8',
+    productId: '8',
     productPriceInCents: '3589',
     productTitle: 'Camiseta Masculina',
     productImageUri: 'product-2',
   },
   {
-    id: '9',
+    productId: '9',
     productPriceInCents: '120090',
     productTitle: 'Sofá',
     productImageUri: 'product-1',
   },
   {
-    id: '10',
+    productId: '10',
     productPriceInCents: '3589',
     productTitle: 'Camiseta Masculina',
     productImageUri: 'product-2',
@@ -74,9 +67,9 @@ const PRODUCTS: Product[] = [
 ]
 
 export default function Home() {
-  const renderProduct: ListRenderItem<Product> = ({ item }) => (
+  const renderProduct: ListRenderItem<ProductCardProps> = ({ item }) => (
     <ProductCard
-      productId={item.id}
+      productId={item.productId}
       productPriceInCents={item.productPriceInCents}
       productTitle={item.productTitle}
       productImageUri={item.productImageUri}
@@ -87,7 +80,7 @@ export default function Home() {
     <FlatList
       data={PRODUCTS}
       renderItem={renderProduct}
-      keyExtractor={(item) => item.id}
+      keyExtractor={(item) => item.productId}
       numColumns={2}
       showsVerticalScrollIndicator={false}
       contentContainerClassName="p-[16px] gap-[8px]"
