48e6a9ad83f0d1797ef8758218ef9ffa97aafed4
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-21 22:07:51 -0300

    feat: remove loading session check from Index component for cleaner redirection logic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,9 @@ import { Redirect } from 'expo-router'
 import { useSession } from '@/contexts/auth-context'
 
 export default function Index() {
-  const { session, isLoadingSession } = useSession()
+  const { session } = useSession()
 
-  // Don't redirect while loading session
-  if (isLoadingSession) {
-    return null
-  }
-
-  // Redirect based on session state
-  // Stack.Protected will enforce these routes
+  // SplashScreenController guarantees session is already loaded
+  // Redirect based on session state - Stack.Protected will enforce these routes
   return session ? <Redirect href="/home" /> : <Redirect href="/login" />
 }
