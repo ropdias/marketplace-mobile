ac649a8dd9ddc4a50ba1790af20555e2f052f258
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-22 23:50:10 -0300

    feat: enhance Login screen layout with keyboard handling and ScrollView

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,6 +1,6 @@
 import { router } from 'expo-router'
 import { useState } from 'react'
-import { Text } from 'react-native'
+import { KeyboardAvoidingView, Platform, ScrollView, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import Logo from '@/assets/logo.svg'
@@ -39,60 +39,75 @@ export default function Login() {
 
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <VStack className="w-full flex-1 items-center justify-between p-[40px]">
-        <VStack className="w-full items-center gap-[61px]">
-          <VStack className="w-full items-center gap-[32px]">
-            <Logo width={64} height={48} />
-            <VStack className="w-full items-center gap-[8px]">
-              <Text className="font-title-lg text-gray-500">
-                Acesse sua conta
-              </Text>
-              <Text className="font-body-sm text-center text-gray-300">
-                Informe seu e-mail e senha para entrar
-              </Text>
+      <KeyboardAvoidingView
+        style={{ flex: 1, width: '100%' }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : 0}
+      >
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          className="w-full"
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          <VStack className="w-full flex-1 items-center justify-between gap-[40px] p-[40px]">
+            <VStack className="w-full items-center gap-[61px]">
+              <VStack className="w-full items-center gap-[32px]">
+                <Logo width={64} height={48} />
+                <VStack className="w-full items-center gap-[8px]">
+                  <Text className="font-title-lg text-gray-500">
+                    Acesse sua conta
+                  </Text>
+                  <Text className="font-body-sm text-center text-gray-300">
+                    Informe seu e-mail e senha para entrar
+                  </Text>
+                </VStack>
+              </VStack>
+              <VStack className="w-full gap-[40px]">
+                <VStack className="w-full gap-[20px]">
+                  <Input isFilled={email.length > 0} label="E-mail">
+                    <InputIcon as={Mail02Icon} />
+                    <InputField
+                      placeholder="mail@exemplo.br"
+                      value={email}
+                      onChangeText={setEmail}
+                      keyboardType="email-address"
+                      autoCapitalize="none"
+                    />
+                  </Input>
+                  <Input isFilled={password.length > 0} label="Senha">
+                    <InputIcon as={AccessIcon} />
+                    <InputField
+                      placeholder="Sua senha"
+                      value={password}
+                      onChangeText={setPassword}
+                      type={showPassword ? 'text' : 'password'}
+                    />
+                    <InputSlot onPress={() => setShowPassword(!showPassword)}>
+                      <InputRightIcon
+                        as={showPassword ? ViewOffIcon : ViewIcon}
+                      />
+                    </InputSlot>
+                  </Input>
+                </VStack>
+                <Button variant="solid" onPress={login}>
+                  <ButtonText>Acessar</ButtonText>
+                  <ButtonIcon as={ArrowRight02Icon} />
+                </Button>
+              </VStack>
             </VStack>
-          </VStack>
-          <VStack className="w-full gap-[40px]">
             <VStack className="w-full gap-[20px]">
-              <Input isFilled={email.length > 0} label="E-mail">
-                <InputIcon as={Mail02Icon} />
-                <InputField
-                  placeholder="mail@exemplo.br"
-                  value={email}
-                  onChangeText={setEmail}
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                />
-              </Input>
-              <Input isFilled={password.length > 0} label="Senha">
-                <InputIcon as={AccessIcon} />
-                <InputField
-                  placeholder="Sua senha"
-                  value={password}
-                  onChangeText={setPassword}
-                  type={showPassword ? 'text' : 'password'}
-                />
-                <InputSlot onPress={() => setShowPassword(!showPassword)}>
-                  <InputRightIcon as={showPassword ? ViewOffIcon : ViewIcon} />
-                </InputSlot>
-              </Input>
+              <Text className="font-body-md text-gray-300">
+                Ainda não tem uma conta?
+              </Text>
+              <Button variant="outline" onPress={register}>
+                <ButtonText>Cadastrar</ButtonText>
+                <ButtonIcon as={ArrowRight02Icon} />
+              </Button>
             </VStack>
-            <Button variant="solid" onPress={login}>
-              <ButtonText>Acessar</ButtonText>
-              <ButtonIcon as={ArrowRight02Icon} />
-            </Button>
           </VStack>
-        </VStack>
-        <VStack className="w-full gap-[20px]">
-          <Text className="font-body-md text-gray-300">
-            Ainda não tem uma conta?
-          </Text>
-          <Button variant="outline" onPress={register}>
-            <ButtonText>Cadastrar</ButtonText>
-            <ButtonIcon as={ArrowRight02Icon} />
-          </Button>
-        </VStack>
-      </VStack>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
