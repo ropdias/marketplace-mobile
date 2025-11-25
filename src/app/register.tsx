491a3b9b8928ffeda380b45eebcd3c0909898c98
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-25 00:29:51 -0300

    feat: implement registration form with image upload and validation

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,8 +1,14 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { File } from 'expo-file-system'
 import { router } from 'expo-router'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { KeyboardAvoidingView, Platform, ScrollView, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { z } from 'zod'
 
+import { uploadImages } from '@/api/attachments/upload-images'
+import { createSeller } from '@/api/sellers/create-seller'
 import Logo from '@/assets/logo.svg'
 import { ImageUploader } from '@/components/image-uploader'
 import { Button, ButtonIcon, ButtonText } from '@/components/ui/button'
@@ -23,20 +29,140 @@ import {
   InputSlot,
 } from '@/components/ui/input'
 import { VStack } from '@/components/ui/vstack'
+import { useAppToast } from '@/hooks/use-app-toast'
+import { AppError } from '@/utils/app-error'
+import { createFormDataImage } from '@/utils/create-image-form-data'
 import { phoneApplyMask } from '@/utils/phone-apply-mask'
 
+const signUpSchema = z
+  .object({
+    name: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres'),
+    phone: z
+      .string()
+      .transform((val) => val.replace(/\D/g, ''))
+      .refine((val) => /^\d{10,11}$/.test(val), {
+        message: 'Telefone inválido, insira DDD e número corretamente',
+      }),
+    email: z.email({ error: 'Email inválido' }),
+    password: z
+      .string()
+      .min(6, { error: 'A senha deve ter no mínimo 6 caracteres' }),
+    passwordConfirmation: z
+      .string()
+      .min(6, { error: 'A senha deve ter no mínimo 6 caracteres' }),
+    profilePicture: z
+      .instanceof(File)
+      .refine((file) => file.type.startsWith('image/'), {
+        message: 'O arquivo precisa ser uma imagem',
+      })
+      .refine((file) => file.size < 5 * 1024 * 1024, {
+        message: 'A imagem deve ter menos de 5MB',
+      })
+      .optional(),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senhas não coincidem',
+    path: ['passwordConfirmation'],
+  })
+
+type SignUpFormData = z.infer<typeof signUpSchema>
+
 export default function Register() {
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [selectedImage, setSelectedImage] = useState<string | null>(null)
   const [showPassword, setShowPassword] = useState(false)
   const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const { showSuccess, showError } = useAppToast()
 
-  function login() {
-    router.navigate('/login')
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: '',
+      phone: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+    },
+  })
+
+  function goToLogin() {
+    router.navigate({ pathname: '/login', params: { email: 'teste' } })
   }
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        let avatarId: string | null = null
+
+        if (data.profilePicture && selectedImage) {
+          // Create form data to upload the image
+          const photoFile = createFormDataImage({
+            uri: selectedImage,
+            fileData: data.profilePicture,
+          }) as any // 'as any' due to React Native FormData typings
+          const userPhotoUploadForm = new FormData()
+          userPhotoUploadForm.append('files', photoFile)
+
+          try {
+            const uploadImagesResponse = await uploadImages({
+              body: { files: userPhotoUploadForm },
+            })
+            avatarId = uploadImagesResponse.attachments[0]?.id ?? null
+          } catch (error) {
+            const isAppError = error instanceof AppError
+
+            const description = isAppError
+              ? error.message
+              : 'Tente novamente mais tarde.'
+
+            showError({
+              title: 'Erro ao realizar upload da imagem de perfil',
+              description,
+            })
+            // Return to avoid creating a seller without an image
+            return
+          }
+        }
+
+        await createSeller({
+          body: {
+            name: data.name,
+            phone: data.phone,
+            email: data.email,
+            password: data.password,
+            passwordConfirmation: data.passwordConfirmation,
+            avatarId,
+          },
+        })
+        showSuccess({
+          title: 'Cadastro realizado com sucesso!',
+          description: 'Agora é só fazer o login!',
+        })
+        router.replace('/login')
+      } catch (error) {
+        const isAppError = error instanceof AppError
+
+        const description = isAppError
+          ? error.message
+          : 'Tente novamente mais tarde.'
+
+        showError({ title: 'Erro ao realizar cadastro', description })
+      }
+    },
+    [showError, showSuccess, selectedImage],
+  )
+
+  useEffect(() => {
+    // Update the profilePicture field in the form when selectedImage changes
+    if (!selectedImage) return
+    const file = new File(selectedImage)
+    if (!file.exists) return
+    setValue('profilePicture', file, { shouldValidate: true })
+  }, [selectedImage, setValue])
 
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
@@ -66,75 +192,140 @@ export default function Register() {
               </VStack>
               <VStack className="w-full gap-[40px]">
                 <VStack className="w-full items-center gap-[20px]">
-                  <ImageUploader />
-                  <Input isFilled={name.length > 0} label="Nome">
-                    <InputIcon as={UserIcon} />
-                    <InputField
-                      placeholder="Seu nome completo"
-                      value={name}
-                      onChangeText={setName}
-                      keyboardType="default"
-                    />
-                  </Input>
-                  <Input isFilled={phone.length > 0} label="Telefone">
-                    <InputIcon as={CallIcon} />
-                    <InputField
-                      placeholder="(00) 00000-0000"
-                      value={phone}
-                      onChangeText={(text) => setPhone(phoneApplyMask(text))}
-                      keyboardType="number-pad"
-                    />
-                  </Input>
+                  <Controller
+                    control={control}
+                    name="profilePicture"
+                    render={({ field: { onChange, value } }) => (
+                      <ImageUploader
+                        selectedImage={selectedImage}
+                        setSelectedImage={setSelectedImage}
+                      />
+                    )}
+                  />
+                  {errors.profilePicture?.message && (
+                    <Text className="font-body-sm text-red-500">
+                      {errors.profilePicture.message}
+                    </Text>
+                  )}
+                  <Controller
+                    control={control}
+                    name="name"
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        isFilled={value.length > 0}
+                        label="Nome"
+                        errorMessage={errors.name?.message}
+                      >
+                        <InputIcon as={UserIcon} />
+                        <InputField
+                          placeholder="Seu nome completo"
+                          onChangeText={onChange}
+                          value={value}
+                          keyboardType="default"
+                        />
+                      </Input>
+                    )}
+                  />
+                  <Controller
+                    control={control}
+                    name="phone"
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        isFilled={value.length > 0}
+                        label="Telefone"
+                        errorMessage={errors.phone?.message}
+                      >
+                        <InputIcon as={CallIcon} />
+                        <InputField
+                          placeholder="(00) 00000-0000"
+                          value={value}
+                          onChangeText={(text) =>
+                            onChange(phoneApplyMask(text))
+                          }
+                          keyboardType="number-pad"
+                        />
+                      </Input>
+                    )}
+                  />
                 </VStack>
                 <VStack className="w-full gap-[20px]">
                   <Text className="font-title-sm text-gray-500">Acesso</Text>
-                  <Input isFilled={email.length > 0} label="E-mail">
-                    <InputIcon as={Mail02Icon} />
-                    <InputField
-                      placeholder="mail@exemplo.br"
-                      value={email}
-                      onChangeText={setEmail}
-                      keyboardType="email-address"
-                      autoCapitalize="none"
-                    />
-                  </Input>
-                  <Input isFilled={password.length > 0} label="Senha">
-                    <InputIcon as={AccessIcon} />
-                    <InputField
-                      placeholder="Sua senha"
-                      value={password}
-                      onChangeText={setPassword}
-                      type={showPassword ? 'text' : 'password'}
-                    />
-                    <InputSlot onPress={() => setShowPassword(!showPassword)}>
-                      <InputRightIcon
-                        as={showPassword ? ViewOffIcon : ViewIcon}
-                      />
-                    </InputSlot>
-                  </Input>
-                  <Input
-                    isFilled={confirmPassword.length > 0}
-                    label="Confirmar Senha"
-                  >
-                    <InputIcon as={AccessIcon} />
-                    <InputField
-                      placeholder="Confirme a senha"
-                      value={confirmPassword}
-                      onChangeText={setConfirmPassword}
-                      type={showConfirmPassword ? 'text' : 'password'}
-                    />
-                    <InputSlot
-                      onPress={() =>
-                        setShowConfirmPassword(!showConfirmPassword)
-                      }
-                    >
-                      <InputRightIcon
-                        as={showConfirmPassword ? ViewOffIcon : ViewIcon}
-                      />
-                    </InputSlot>
-                  </Input>
+                  <Controller
+                    control={control}
+                    name="email"
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        isFilled={value.length > 0}
+                        label="E-mail"
+                        errorMessage={errors.email?.message}
+                      >
+                        <InputIcon as={Mail02Icon} />
+                        <InputField
+                          placeholder="mail@exemplo.br"
+                          value={value}
+                          onChangeText={onChange}
+                          keyboardType="email-address"
+                          autoCapitalize="none"
+                        />
+                      </Input>
+                    )}
+                  />
+                  <Controller
+                    control={control}
+                    name="password"
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        isFilled={value.length > 0}
+                        label="Senha"
+                        errorMessage={errors.password?.message}
+                      >
+                        <InputIcon as={AccessIcon} />
+                        <InputField
+                          placeholder="Sua senha"
+                          value={value}
+                          onChangeText={onChange}
+                          type={showPassword ? 'text' : 'password'}
+                        />
+                        <InputSlot
+                          onPress={() => setShowPassword(!showPassword)}
+                        >
+                          <InputRightIcon
+                            as={showPassword ? ViewOffIcon : ViewIcon}
+                          />
+                        </InputSlot>
+                      </Input>
+                    )}
+                  />
+                  <Controller
+                    control={control}
+                    name="passwordConfirmation"
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        isFilled={value.length > 0}
+                        label="Confirmar Senha"
+                        errorMessage={errors.passwordConfirmation?.message}
+                      >
+                        <InputIcon as={AccessIcon} />
+                        <InputField
+                          placeholder="Confirme a senha"
+                          value={value}
+                          onChangeText={onChange}
+                          type={showConfirmPassword ? 'text' : 'password'}
+                        />
+                        <InputSlot
+                          onPress={() =>
+                            setShowConfirmPassword(!showConfirmPassword)
+                          }
+                        >
+                          <InputRightIcon
+                            as={showConfirmPassword ? ViewOffIcon : ViewIcon}
+                          />
+                        </InputSlot>
+                      </Input>
+                    )}
+                  />
                 </VStack>
-                <Button variant="solid" onPress={login}>
+                <Button variant="solid" onPress={handleSubmit(handleSignUp)}>
                   <ButtonText>Cadastrar</ButtonText>
                   <ButtonIcon as={ArrowRight02Icon} />
                 </Button>
@@ -144,7 +335,7 @@ export default function Register() {
               <Text className="font-body-md text-gray-300">
                 Já tem uma conta?
               </Text>
-              <Button variant="outline" onPress={login}>
+              <Button variant="outline" onPress={goToLogin}>
                 <ButtonText>Acessar</ButtonText>
                 <ButtonIcon as={ArrowRight02Icon} />
               </Button>
--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -1,17 +1,22 @@
-import * as Crypto from 'expo-crypto'
-import { File } from 'expo-file-system'
 import * as ImagePicker from 'expo-image-picker'
-import { useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 
-import { ToastMessage } from './toast-message'
+import { useAppToast } from '@/hooks/use-app-toast'
+import { AppError } from '@/utils/app-error'
+
 import { Icon, ImageUploadIcon } from './ui/icon'
 import { Image } from './ui/image'
-import { useToast } from './ui/toast'
 
-export function ImageUploader() {
-  const toast = useToast()
-  const [selectedImage, setSelectedImage] = useState<string | null>(null)
+interface ImageUploaderProps {
+  selectedImage: string | null
+  setSelectedImage: React.Dispatch<React.SetStateAction<string | null>>
+}
+
+export function ImageUploader({
+  selectedImage,
+  setSelectedImage,
+}: ImageUploaderProps) {
+  const { showError } = useAppToast()
 
   async function handleUserPhotoSelect() {
     try {
@@ -29,90 +34,19 @@ export function ImageUploader() {
       const photoUri = photoSelected.assets[0].uri
 
       if (photoUri) {
-        const file = new File(photoUri)
-        if (!file.exists) return
-
-        if (file.size / 1024 / 1024 > 5) {
-          return toast.show({
-            placement: 'top',
-            duration: 3000,
-            render: ({ id }) => {
-              return (
-                <ToastMessage
-                  id={id}
-                  action="error"
-                  title="Imagem muito grande!"
-                  description="Escolha uma imagem de até 5MB."
-                />
-              )
-            },
-          })
-        }
-
         setSelectedImage(photoUri)
-
-        const fileExtension = photoSelected.assets[0].uri.split('.').pop()
-
-        const photoFile = {
-          name: `${Crypto.randomUUID()}.${fileExtension}`,
-          uri: photoSelected.assets[0].uri,
-          type: photoSelected.assets[0].mimeType || `image/${fileExtension}`,
-        } as any
-
-        const userPhotoUploadForm = new FormData()
-
-        userPhotoUploadForm.append('avatar', photoFile)
-
-        // const avatarUpdtedResponse = await api.patch(
-        //   '/users/avatar',
-        //   userPhotoUploadForm,
-        //   {
-        //     headers: {
-        //       'Content-Type': 'multipart/form-data',
-        //     },
-        //   },
-        // )
-
-        // const userUpdated = user
-
-        // userUpdated.avatar = avatarUpdtedResponse.data.avatar
-
-        // await updateUserProfile(userUpdated)
-
-        toast.show({
-          placement: 'top',
-          duration: 3000,
-          render: ({ id }) => {
-            return (
-              <ToastMessage
-                id={id}
-                title="Sucesso!"
-                description="Foto atualizada"
-                action="success"
-              />
-            )
-          },
-        })
+      } else {
+        throw new AppError('Não foi possível selecionar a imagem.')
       }
-    } catch {
-      //   const isAppError = error instanceof AppError
-      //   const description = isAppError
-      //     ? error.message
-      //     : 'Não foi possível atualizar a imagem do perfil. Tente novamente mais tarde.'
-      //   toast.show({
-      //     placement: 'top',
-      //     duration: 3000,
-      //     render: ({ id }) => {
-      //       return (
-      //         <ToastMessage
-      //           id={id}
-      //           action="error"
-      //           title="Erro ao atualizar a imagem do perfil"
-      //           description={description}
-      //         />
-      //       )
-      //     },
-      //   })
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const description = isAppError
+        ? error.message
+        : 'Não foi possível atualizar a imagem do perfil. Tente novamente mais tarde.'
+      showError({
+        title: 'Erro ao selecionar a imagem do perfil',
+        description,
+      })
     }
   }
 
