0c1c109c3da7076078dbf0e68e3b6ef6810bd109
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-25 00:52:19 -0300

    feat: add update seller API endpoint with request and response types

---
+++ src/api/sellers/update-seller.ts
@@ -0,0 +1,42 @@
+import { Seller } from '@/@types/seller'
+import { api } from '@/lib/axios'
+
+export interface updateSellerBody {
+  name: string
+  phone: string
+  email: string
+  avatarId?: string | null
+  password?: string
+  newPassword?: string
+}
+
+export interface UpdateSellerResponse {
+  seller: Seller
+}
+
+export interface UpdateSellerProps {
+  body: updateSellerBody
+  accessToken: string
+}
+
+export async function updateSeller({ body, accessToken }: UpdateSellerProps) {
+  const { name, phone, email, avatarId, password, newPassword } = body
+
+  const response = await api.put<UpdateSellerResponse>(
+    '/sellers',
+    {
+      name,
+      phone,
+      email,
+      avatarId,
+      password,
+      newPassword,
+    },
+    {
+      headers: {
+        Cookie: accessToken,
+      },
+    },
+  )
+  return response.data
+}
